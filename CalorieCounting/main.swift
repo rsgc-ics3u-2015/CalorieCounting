6d56a31079e01624c74dc363be95237c78ad9338
Russell Gordon <rgordon@rsgc.on.ca>
2016-05-10 05:00:56 -0400

    Side order working

--- CalorieCounting/main.swift
+++ CalorieCounting/main.swift
@@ -45,5 +45,37 @@ repeat {
     
 } while burgerChoice == -1
 
-print(burgerChoice)
+/*
+ * Get the burger choice
+ */
+var sideOrderChoice : Int = -1
+
+// Loop until valid input provided
+repeat {
+    
+    // Show prompt
+    print("Please enter a side order choice: ", terminator: "")
+    
+    // Wait for input
+    var givenInput = readLine(stripNewline: true)
+    
+    // Use optional casting to get a String, if possible
+    if let stringInput = givenInput {
+        
+        // Now we have a String, try to make it an Int
+        if let integerInput = Int(stringInput) {
+            
+            // Now we have an integer, see if it's in the acceptable range (1 to 4 inclusive)
+            if integerInput > 0 && integerInput < 5 {
+                
+                // We have valid input, so save it
+                sideOrderChoice = integerInput
+                
+            }
+            
+        }
+        
+    }
+    
+} while sideOrderChoice == -1
 
