e30a72f544edb70b1152a60318292247316498a4
Russell Gordon <rgordon@rsgc.on.ca>
2016-05-10 05:02:31 -0400

    Drink choice working

--- CalorieCounting/main.swift
+++ CalorieCounting/main.swift
@@ -79,3 +79,40 @@ repeat {
     
 } while sideOrderChoice == -1
 
+/*
+ * Get the drink choice
+ */
+var drinkChoice : Int = -1
+
+// Loop until valid input provided
+repeat {
+    
+    // Show prompt
+    print("Please enter a drink choice: ", terminator: "")
+    
+    // Wait for input
+    var givenInput = readLine(stripNewline: true)
+    
+    // Use optional casting to get a String, if possible
+    if let stringInput = givenInput {
+        
+        // Now we have a String, try to make it an Int
+        if let integerInput = Int(stringInput) {
+            
+            // Now we have an integer, see if it's in the acceptable range (1 to 4 inclusive)
+            if integerInput > 0 && integerInput < 5 {
+                
+                // We have valid input, so save it
+                drinkChoice = integerInput
+                
+            }
+            
+        }
+        
+    }
+    
+} while drinkChoice == -1
+
+
+
+
