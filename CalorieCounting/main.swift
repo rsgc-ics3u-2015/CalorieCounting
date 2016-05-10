5d3a08f2f601be62f6e571a84e6d39593850edc0
Russell Gordon <rgordon@rsgc.on.ca>
2016-05-10 04:59:35 -0400

    Got the burger choice working

---
+++ CalorieCounting/main.swift
@@ -0,0 +1,49 @@
+//
+//  main.swift
+//  CalorieCounting
+//
+//  Created by Russell Gordon on 2016-05-10.
+//  Copyright © 2016 Royal St. George's College. All rights reserved.
+//
+
+import Foundation
+
+// Print the welcome message
+print("Welcome to Chip's Fast Food Emporium")
+
+/* 
+ * Get the burger choice
+ */
+var burgerChoice : Int = -1
+
+// Loop until valid input provided
+repeat {
+    
+    // Show prompt
+    print("Please enter a burger choice: ", terminator: "")
+    
+    // Wait for input
+    var givenInput = readLine(stripNewline: true)
+    
+    // Use optional casting to get a String, if possible
+    if let stringInput = givenInput {
+        
+        // Now we have a String, try to make it an Int
+        if let integerInput = Int(stringInput) {
+            
+            // Now we have an integer, see if it's in the acceptable range (1 to 4 inclusive)
+            if integerInput > 0 && integerInput < 5 {
+                
+                // We have valid input, so save it
+                burgerChoice = integerInput
+                
+            }
+            
+        }
+        
+    }
+    
+} while burgerChoice == -1
+
+print(burgerChoice)
+
